57d2f0e1a16777b898ab36b44c9863910cab37e4
khoathuong203 <144438309+khoathuong203@users.noreply.github.com>
2025-03-05 10:22:51 +0700

    Phân trang User

--- demo_02/demo_02/Pages/Users/Index.cshtml.cs
+++ demo_02/demo_02/Pages/Users/Index.cshtml.cs
@@ -1,23 +1,44 @@
 ﻿using demo_02.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace demo_02.Pages.Users
 {
     public class IndexModel : PageModel
     {
-        private readonly EventManagementContext _context;
+        private readonly EventService _eventService;
 
-        public IndexModel(EventManagementContext context)
+        public IndexModel(EventService eventService)
         {
-            _context = context;
+            _eventService = eventService;
         }
 
-        public IList<User> Users { get; set; }
+        public List<User> Users { get; set; }
+        public Dictionary<int, string> RoleDictionary { get; set; } = new();
 
         public async Task OnGetAsync()
         {
-            Users = await _context.Users.Include(u => u.Roles).ToListAsync();
+            // Lấy danh sách người dùng
+            Users = await _eventService.GetAllUsersAsync();
+
+            // Lấy danh sách vai trò và tạo Dictionary để tra cứu nhanh
+            var roles = await _eventService.GetRolesAsync();
+            RoleDictionary = roles.ToDictionary(r => r.RolesId, r => r.Name);
+
+            // Gán tên vai trò cho từng người dùng
+            foreach (var user in Users)
+            {
+                if (RoleDictionary.TryGetValue(user.RolesId, out var roleName))
+                {
+                    user.Roles = new Role { Name = roleName }; // Gán thủ công vì chỉ có RolesId trong User
+                }
+                else
+                {
+                    user.Roles = new Role { Name = "Không xác định" }; // Tránh lỗi null
+                }
+            }
         }
     }
 }
