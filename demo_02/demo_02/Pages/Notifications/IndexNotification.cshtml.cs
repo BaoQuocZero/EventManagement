63710cd2afd0f961d95c814bb5fff7f745bd2946
Quốc Bảo <baoquoczero@gmail.com>
2025-03-24 13:34:48 +0700

    Hiển thị thông báo

---
+++ demo_02/demo_02/Pages/Notifications/IndexNotification.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using demo_02.Models;
+using demo_02.Servers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace demo_02.Pages.Notifications
+{
+    public class IndexNotificationModel : PageModel
+    {
+        private readonly NotificationService _notificationService;
+
+        public IndexNotificationModel(NotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public List<Notification> Notifications { get; set; } = new List<Notification>();
+
+        public async Task OnGetAsync()
+        {
+            Notifications = await _notificationService.GetAllNotificationsAsync();
+        }
+
+        [BindProperty]
+        public int? DeleteId { get; set; } // Lưu ID sự kiện cần xóa
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (DeleteId.HasValue)
+            {
+                var success = await _notificationService.DeleteNotificationAsync(DeleteId.Value);
+                if (!success)
+                {
+                    ModelState.AddModelError("", "Không thể xóa.");
+                }
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
